335bbb4e4001309f1b3f355b5a4c829cc5afaf15
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-07-24 20:50:57 -0300

    feat: patients list

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,6 +1,13 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.patient.Patient;
+import med.voll.api.patient.PatientListRecord;
 import med.voll.api.patient.PatientRepository;
 import med.voll.api.patient.RegisterPatientRecord;
 
@@ -22,5 +30,18 @@ public class PatientController {
   public void register(@RequestBody @Valid RegisterPatientRecord data) {
     Patient patient = new Patient(data);
     patientRepository.save(patient);
+  }
+
+  @GetMapping
+  public Page<PatientListRecord> list(
+    @PageableDefault(
+      size = 5,
+      page = 0,
+      sort = {"cpf"},
+      direction =  Direction.ASC
+    )
+    Pageable pageable) {
+      return patientRepository.findAll(pageable).map(PatientListRecord::new);
+
   }
 }
