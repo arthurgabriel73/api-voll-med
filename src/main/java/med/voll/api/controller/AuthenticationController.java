9ac94edd45705e6eeb86a88dd6432af8a10acee8
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-15 18:31:49 -0300

    feat: configures spring security

---
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.user.LoginRecord;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+  
+  @Autowired
+  private AuthenticationManager manager;
+
+  @PostMapping
+  public ResponseEntity doLogin(@RequestBody @Valid LoginRecord loginData) { 
+    var token = new UsernamePasswordAuthenticationToken(loginData.login(), loginData.password());
+    var authentication = manager.authenticate(token);
+
+    return ResponseEntity.ok().build();
+  }
+}
---
+++ src/main/java/med/voll/api/domain/user/LoginRecord.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.user;
+
+public record LoginRecord (String login, String password) {
+}
