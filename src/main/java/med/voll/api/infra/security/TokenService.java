a0ec51743cc67ea14c8082067d621a80612f789c
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-17 00:35:53 -0300

    feat: implements user authentication by token

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import med.voll.api.domain.user.User;
 
@@ -30,6 +31,20 @@ public class TokenService {
         .sign(algorithm);
     } catch (JWTCreationException exception){
         throw new RuntimeException("Error: unable to generate token.");
+    }
+  }
+
+  public String getSubject(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+          .withIssuer("API Voll.med")
+          .build()
+          .verify(token)
+          .getSubject();
+
+    } catch (JWTVerificationException exception) {
+      throw new RuntimeException("Invalid Token!");
     }
   }
 
